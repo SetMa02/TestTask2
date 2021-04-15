1ef1332953aea1f6559478f49c45f0345818108e
SetMa02 <maxuser99@mail.ru>
2021-04-15 19:52:54 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask2
+{
+
+    class Cycler
+    {
+        private List<int> numbers;
+
+        public Cycler(List<int> numbers)
+        {
+            this.numbers = numbers;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.AddRange(new int[] { 5, 3, 2, 1 });
+            Cycler cycler = new Cycler(numbers);
+        }
+    }
+
+    
+}
